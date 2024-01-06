6f2745ef37d0d7de25f97c380403ebbee9c9d023
parag477 <paragagrawal477@gmail.com>
2024-01-07 02:01:12 +0530

    Commit #3 - One way connection 🐌

--- Realtime-Chat/apps/web/context/SocketProvider.tsx
+++ Realtime-Chat/apps/web/context/SocketProvider.tsx
@@ -1,6 +1,7 @@
 "use client"
-import React, { useCallback, useContext, useEffect } from "react";
-import { io } from "socket.io-client";
+import { fdatasync } from "fs";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { io,Socket } from "socket.io-client";
 
 interface SocketProviderProps {
     children ?: React.ReactNode;
@@ -21,15 +22,22 @@ export const useSocket = () => {
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
 
+    const [socket, setSocket] = useState<Socket>()
+
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("Send Message", msg);
-    }, []);
+        if (socket) {
+            socket.emit('event:message', { message: msg })
+        }
+    }, [socket]);
 
     useEffect(() => {
         const _socket = io('http://localhost:8000');
+        setSocket(_socket)
 
         return () => {
             _socket.disconnect();
+            setSocket(undefined);
         };
     }, []);
 
